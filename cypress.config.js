0fcaaf2768937f04cb822f1fd740e14b5e1648f2
Tiago Reis <tiagoreisaraujo7@gmail.com>
2025-03-14 10:51:35 -0300

    Add Curso de Cypress-do-zero-a-Nuvem

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportHeight: 880,
+  viewportWidth: 1280,
+  e2e: {},
+});
---
+++ cypress/e2e/CAC-TAT.cy.js
@@ -0,0 +1,93 @@
+describe("Central de Atendimento ao Cliente TAT", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html");
+  });
+
+  it("verifica o título da aplicação", () => {
+    cy.title().should("include", "Central de Atendimento ao Cliente TAT");
+  });
+
+  //Exercício extra 1
+  it("preenche os campos obrigatórios e enviar o formulário", () => {
+    const longText = Cypress._.repeat("abcdefghijklmnopqrstuvwxyz", 10); // criou uma varialvel com texto longo
+
+    cy.get("#firstName").type("Tiago"); // preenche o campo de nome
+    cy.get("#lastName").type("Reis"); // preenche o campo de sobrenome
+    cy.get("#email").type("tiago7@my.com"); // preenche o campo de email
+    //cy.get('#phone').type('71981912552') // preenche o campo de telefone
+    cy.get("#open-text-area").type(longText, { delay: 0 }); // preenche o campo de mensagem com a string criada acima, com delay 0 para preencher mais rápido o campo de texto grande (text area)
+    cy.get('button[type="submit"]').click(); // pega o seletor css do button, fazendo um click no botão de submit do formulário
+
+    cy.get(".success").should("be.visible"); // verifica se a mensagem de sucesso está visível
+  });
+
+  //Exercício extra 2
+  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
+    cy.get("#firstName").type("Tiago");
+    cy.get("#lastName").type("Reis");
+    cy.get("#email").type("tiago7@my,com");
+    cy.get("#open-text-area").type("Teste");
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  //Exercício extra 3
+  it("campo telefone continua vazio quando preenchido com um valor não-numérico", () => {
+    cy.get("#phone") // pega o campo de telefone e preenche com um valor não numérico (string)
+      .type("abcdedfghij") // preenche o campo de telefone com uma string não numérica (letras)
+      .should("have.value", ""); // verifica se o campo de telefone está vazio após preencher com uma string não numérica (letras)
+  });
+
+  //Exercício extra 4
+  it("exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário", () => {
+    cy.get("#firstName").type("Tiago");
+    cy.get("#lastName").type("Reis");
+    cy.get("#email").type("tiago7@my,com");
+    cy.get("#open-text-area").type("Teste");
+    cy.get("#phone-checkbox").click();
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+
+  //Exercício extra 5
+  it("preenche e limpa os campos nome, sobrenome, email e telefone", () => {
+    cy.get("#firstName")
+      .type("Tiago")
+      .should("have.value", "Tiago")
+      .clear()
+      .should("have.value", "");
+
+    cy.get("#lastName")
+      .type("Reis")
+      .should("have.value", "Reis")
+      .clear()
+      .should("have.value", "");
+
+    cy.get("#email")
+      .type("tiago7@my.com")
+      .should("have.value", "tiago7@my.com")
+      .clear()
+      .should("have.value", "");
+
+    cy.get("#phone")
+      .type("71981912552")
+      .should("have.value", "71981912552")
+      .clear()
+      .should("have.value", "");
+  });
+
+  //Exercício extra 6
+  it("exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios", () => {
+    cy.get('button[type="submit"]').click();
+
+    cy.get(".error").should("be.visible");
+  });
+  //Exercício extra 7
+
+  it.only("envia o formuário com sucesso usando um comando customizado", () => {
+    cy.fillMandatoryFieldsAndSubmit();
+    cy.get(".success").should("be.visible");
+  });
+});
